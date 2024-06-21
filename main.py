df5f8ad26c76685d02cb92c558500d92d0f26380
Dragon267 <112898086+Dragon267@users.noreply.github.com>
2024-06-21 08:11:51 +0200

    example coloring of spheres

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import numpy as np
 ball = BallModel()
 
 ball.add_object(PoincareSphere([6, 6, 4], 1))
-ball.add_object(PoincareSphere([5, 6, 3], 1))
+ball.add_object(PoincareSphere([5, 6, 3], 1, color=(34, 255, 0)))
 
 camera = PoincareCamera(PoincarePoint(4, 3, 2), EPS=1/20)
 
 ball.set_camera(camera)
 
-# plot_hyperbolic(ball, show_camera_view=False)
+plot_hyperbolic(ball, show_camera_view=False)
 
 pixels = get_world_view(ball, camera)
 
